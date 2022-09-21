bb6d308b176658d29a29618746fb27d704cb6296
tatamanduca <tatamanduca2@gmail.com>
2022-09-21 18:03:05 -0300

    cenario cadastro de usuarios automatizado

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,27 +1,11 @@
 
-#step1
 Dado('que acesso a pagina de cadastro') do                     #acesso a página de cadastro                        
-    visit "http://rocklov-web:3000/signup"
+    @signup_page.visit_page
 end                                                                            
                                                                                  
 Quando('submeto o seguinte formnulário de cadastro:') do |table|  
-    
     user = table.hashes.first
-
     MongoDB.new.remove_user(user[:email])
 
-    find("#fullName").set user[:nome]
-    find("#email").set user[:email]                       
-    find("#password").set user[:senha]
-
-    click_button "Cadastrar"
-end                                                                            
-                                                                                 
-Então('sou redirecionado para o Dashboard') do                 #espero que na pagina de cadastro tenha
-    expect(page).to have_css ".dashboard"                      #o elemento que gante que fui pra área logada
-end                                                                                      
-                                                                                 
-Então('vejo a mensagem de alerta: {string}') do |expect_alert|
-    alert = find(".alert-dark")
-    expect(alert.text).to eql expect_alert
-end                                                                           
+    @signup_page.create(user)
+end                                                                                                                                                     
