01a8ceffa5a639bffda0740763842ddd7261ed9b
tatamanduca <tatamanduca2@gmail.com>
2022-09-21 18:07:10 -0300

    suporte

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,4 +4,6 @@ require "faker"
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
+    config.app_host = "http://rocklov-web:3000"
+    config.default_max_wait_time = 7
 end
---
+++ features/support/hooks.rb
@@ -0,0 +1,10 @@
+
+Before do
+    @alert = Alert.new
+    @login_page = LoginPage.new
+    @signup_page = SignupPage.new
+    @dash_page = DashPage.new
+    @equipos_page = EquiposPage.new
+
+    page.current_window.resize_to(1440, 900)
+end
