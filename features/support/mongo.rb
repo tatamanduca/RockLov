31db31b6e7f8805e98fcacf62f72ce3851efb033
tatamanduca <tatamanduca2@gmail.com>
2022-09-21 18:04:55 -0300

    mongo configurado

--- features/support/mongo.rb
+++ features/support/mongo.rb
@@ -3,11 +3,25 @@ require 'mongo'                                                           #para 
 Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class MongoDB
-#método    
-    def remove_user(email)                                                #email = argumento 
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')  #guarda a conexão na variavel client
-        users = client[:users]                                            #exclui este email do banco de dados antes de preencher o cadastro
-        users.delete_many({email: email})  
+    attr_accessor :users, :equipos
+
+    def initialize
+        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+        @users = client[:users]
+        @equipos = client[:equipos]
     end
    
+    def remove_user(email)                                                #email = argumento                                            
+        @users.delete_many({email: email})  
+    end
+   
+    def get_user(email)                                           
+        user = @users.find({email: email}).first
+        return user[:_id]
+    end
+
+    def remove_equipo(name, email)
+        user_id = get_user(email)                                      
+        @equipos.delete_many({name: name, user: user_id})  
+    end
 end
