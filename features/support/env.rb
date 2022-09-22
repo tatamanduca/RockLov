09fb9ab894da3e4bf10cb9bf46030887c2124e03
tatamanduca <tatamanduca2@gmail.com>
2022-09-22 16:51:10 -0300

    ambiente de execuçao multi browser

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,30 @@
+require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"
 require "faker"
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
+
+BROWSER = ENV["BROWSER"]
+
+case ENV["BROWSER"]
+when "firefox"
+    @driver = :selenium
+when "chrome"
+    @driver = :selenium_chrome
+when "fire_headless"
+    @driver = :selenium_headless
+when "chrome_headless"
+    @driver = :selenium_chrome_headless
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = "http://rocklov-web:3000"
+    config.default_driver = @driver
+    config.app_host = CONFIG["url"]
     config.default_max_wait_time = 7
+end
+
+AllureCucumber.configure do |config|
+    config.results_directory = "/logs"
+    config.clean_results_directory = true
 end
